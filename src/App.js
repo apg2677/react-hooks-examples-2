d507b503a5c0f79dd8951594f0023f4a58644d47
apg2677 <apourrezaei@gmail.com>
2025-08-30 15:37:52 -0400

    init commit 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { useEffect, useState } from "react";
 
 function App() {
+  const [name, setName] = useState(() => {
+    return localStorage.getItem("name") || "";
+  });
+
+  useEffect(() => {
+    localStorage.setItem("name", name);
+  }, [name]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Enter your name: </h2>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <h3>Name: {name} </h3>
     </div>
   );
 }
